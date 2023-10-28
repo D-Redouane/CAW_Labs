94dca60968e82ae1a9646488476a88a363b4708a
D-Redouane <redouanedaddi316@gmail.com>
2023-10-28 16:02:31 +0100

    fixing the error of the game of holding the mouse

--- Lab2/Lab2_maze.js
+++ Lab2/Lab2_maze.js
@@ -4,59 +4,74 @@ document.addEventListener("DOMContentLoaded", function () {
     var boundary = document.querySelectorAll(".boundary");
     var status = document.getElementById("status");
     var gameStarted = false;
+    var holdingMouse = false;
 
-    // Add mouseover event listener to each boundary
-    for (var i = 0; i < boundary.length; i++) {
-    boundary[i].addEventListener("mouseover", function () {
-        if (gameStarted) {
-        this.classList.add("youlose");
-        // Make all boundaries red on hover
-        for (var j = 0; j < boundary.length; j++) {
-            boundary[j].classList.add("youlose");
+    // Function to start the game when 'S' is held (not clicked)
+    document.addEventListener("mousedown", function (event) {
+        if (event.button === 0 && event.target.id === 'start') {
+            gameStarted = true;
+            holdingMouse = true;
+            status.textContent = "Move your mouse to the end to win!";
+            for (var i = 0; i < boundary.length; i++) {
+                boundary[i].classList.remove("youlose");
+            }
         }
-        status.textContent = "You lose!";
+    });
+
+    // Function to handle the release of the left mouse button
+    document.addEventListener("mouseup", function (event) {
+        if (event.button === 0) {
+            holdingMouse = false;
+            if (gameStarted) {
+                for (var j = 0; j < boundary.length; j++) {
+                    boundary[j].classList.add("youlose");
+                }
+                status.textContent = "You lose!";
+                gameStarted = false;
+            }
         }
     });
-    }
 
-    start.addEventListener("mouseover", function () {
-    gameStarted = true;
-    status.textContent = "Move your mouse to the end to win!";
-    // Reset boundaries to normal color
+    // Add mouseover event listener to each boundary when the game is in progress and the mouse is held
     for (var i = 0; i < boundary.length; i++) {
-        boundary[i].classList.remove("youlose");
+        boundary[i].addEventListener("mouseover", function () {
+            if (gameStarted && holdingMouse) {
+                this.classList.add("youlose");
+                for (var j = 0; j < boundary.length; j++) {
+                    boundary[j].classList.add("youlose");
+                }
+                status.textContent = "You lose!";
+                gameStarted = false;
+            }
+        });
     }
-    });
-
-    start.addEventListener("click", function () {
-    gameStarted = false;
-    status.textContent = "Move your mouse over the 'S' to begin.";
-    // Reset boundaries to normal color
-    for (var i = 0; i < boundary.length; i++) {
-        boundary[i].classList.remove("youlose");
-    }
-    });
 
     end.addEventListener("mouseover", function () {
-    if (gameStarted) {
-        status.textContent = "You win!";
-        gameStarted = false;
-    }
+        if (gameStarted && holdingMouse) {
+            for (var j = 0; j < boundary.length; j++) {
+                boundary[j].classList.add("youwin");
+            }
+            status.textContent = "You win!";
+            gameStarted = false;
+        }
     });
 
+    // Function to check if the mouse goes outside the maze area
     document.body.addEventListener("mousemove", function (event) {
-    if (gameStarted) {
-        var mazeRect = document.getElementById("maze").getBoundingClientRect();
-        if (
-        event.clientX < mazeRect.left ||
-        event.clientX > mazeRect.right ||
-        event.clientY < mazeRect.top ||
-        event.clientY > mazeRect.bottom
-        ) {
-        status.textContent = "You lose!";
-        gameStarted = false;
+        if (gameStarted && holdingMouse) {
+            var mazeRect = document.getElementById("maze").getBoundingClientRect();
+            if (
+                event.clientX < mazeRect.left ||
+                event.clientX > mazeRect.right ||
+                event.clientY < mazeRect.top ||
+                event.clientY > mazeRect.bottom
+            ) {
+                for (var j = 0; j < boundary.length; j++) {
+                    boundary[j].classList.add("youlose");
+                }
+                status.textContent = "You lose!";
+                gameStarted = false;
+            }
         }
-    }
     });
 });
-  
