fbb1789ab3c4669e3548f43f08d140b5a414c482
D-Redouane <redouanedaddi316@gmail.com>
2024-01-07 11:47:39 +0100

    MiniProject init

---
+++ lab7 V2/src/components/nav-bar.jsx
@@ -0,0 +1,82 @@
+import React from 'react';
+
+const NavBar = () => {
+  const navbar = React.useRef();
+  const links = document.querySelectorAll(".links a");
+
+  if (links) {
+     links.forEach((link) =>
+       link.addEventListener("click", () => {
+         if (navbar) toggleMenu();
+       })
+     );
+   }
+
+  const toggleMenu = () => {
+    navbar.current.classList.toggle("open");
+    document.body.classList.toggle("stop-scrolling");
+  }
+
+  const NavData={
+    // logo:"./img/logo.png",
+    logo:"./img/logo2.png",
+
+    navButtons:[
+      {
+        link:"/",
+        text:"Home"
+      },
+      {
+        link:"/Services",
+        text:"Services"
+      },
+      {
+        link:"/Portfolio",
+        text:"Portfolio"
+      },
+      {
+        link:"/About",
+        text:"About"
+      },
+      {
+        link:"/Testimonials",
+        text:"Testimonials"
+      },
+      {
+        link:"/Contact",
+        text:"Contact"
+      },
+      {
+        link:"/HireMe",
+        text:"HireMe",
+        class:"active"
+      },
+    ]
+  }
+
+  return (
+    <>
+      <nav ref={navbar}>
+        <div className="container">
+          <div className="logo">
+            <img src={NavData.logo} alt=""/>
+          </div>
+
+          <div className="links">
+            <ul>
+            {NavData.navButtons.map((element) => {
+              return <li key={element.text}><a href={element.link} className={element.class}>{element.text}</a></li>;
+            })}
+            </ul>
+          </div>
+
+          <div className="hamburger-menu" onClick={toggleMenu}>
+            <div className="bar"></div>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+}
+
+export default NavBar;
